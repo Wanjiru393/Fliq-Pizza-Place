7b92573254344655808ab7b553ab6f049bd18d92
Wanjiru393 <carolciru393@gmail.com>
2022-03-19 16:26:07 +0300

    Change of code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-function Pizza(Size,toppings,crust){
-    this.size=size;
-    this.toppings=toppings;
-    this.crust=crust;
-}
-Pizza.prototype.addCost =function(){
-    return parseInt(this.size) + parseInt(this.toppings) +parseInt(this.crust)
-}
+$(document).ready(function(){
+    $("btn.order").click(function(){
+        let size=$("size option:selected").val();
+        let toppings=$("topings option:selected").val();
+        let crust=$("crust option:selected").val();
+
+    function Pizza(size, toppings, crust,) {
+      this.size = size;
+      this.toppings = toppings;
+      this.crust = crust;
+      
+    })
+    Pizza.prototype.addCost =function(){
+        return parseInt(size) + parseInt(toppings) + parseInt(crust);
+      
+    }
+
+}); 
