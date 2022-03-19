2af7576d3ee3e49b7690178cb9e07a1846ebac69
Wanjiru393 <carolciru393@gmail.com>
2022-03-19 22:56:32 +0300

    Addition to js/script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
-$(document).ready(function(){
-    $("btn.order").click(function(){
-        let size=$("size option:selected").val();
-        let toppings=$("topings option:selected").val();
-        let crust=$("crust option:selected").val();
+$(document).ready(function(){    
+    $("#form").submit(function(event){
+        event.preventDefault()
+        let size = parseInt($("#size").val());
+        let toppings =parseInt($("#toppings").val());
+        let crust =parseInt($("#crust").val());
+        function Pizza(size,toppings,crust){
+        this.size = size;
+        this.toppings =toppings;
+        this.crust =crust;
 
-    function Pizza(size, toppings, crust,) {
-      this.size = size;
-      this.toppings = toppings;
-      this.crust = crust;
-      
-    })
-    Pizza.prototype.addCost =function(){
-        return parseInt(size) + parseInt(toppings) + parseInt(crust);
-      
     }
+    let newPizza=new Pizza(size,toppings,crust);
+    
+    Pizza.prototype.addCost=function(){
+        return this.size + this.toppings + this.crust
+    }
+    console.log(newPizza);
+    console.log(newPizza.addCost());
+    $("tbody").append(`<tr>
+                    <th scope="row">1</th>
+                    <td>${newPizza.size}</td>
+                    <td>${newPizza.toppings}</td>
+                    <td>${newPizza.crust}</td>
+                    <td>${newPizza.addCost()}</td>
+                    </tr>`)
 
-}); 
+    });
+    
+});
