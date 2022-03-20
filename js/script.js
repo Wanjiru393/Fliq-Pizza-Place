60ad4a985a47ee5e0f0999c4eea56bf4313e21a3
Wanjiru393 <carolciru393@gmail.com>
2022-03-20 23:06:51 +0300

    Addition of prompts

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
-$(document).ready(function(){    
+$(document).ready(function(){ 
+    let orderNumber =0 
+    let totalCost =0  
     $("#form").submit(function(event){
         event.preventDefault()
         let size = parseInt($("#size").val());
         let toppings =parseInt($("#toppings").val());
         let crust =parseInt($("#crust").val());
+        
         function Pizza(size,toppings,crust){
         this.size = size;
         this.toppings =toppings;
         this.crust =crust;
-
+        
     }
     let newPizza=new Pizza(size,toppings,crust);
-    
+
     Pizza.prototype.addCost=function(){
         return this.size + this.toppings + this.crust
     }
     console.log(newPizza);
     console.log(newPizza.addCost());
+    orderNumber =orderNumber + 1
+    totalCost = totalCost + newPizza.addCost();
     $("tbody").append(`<tr>
-                    <th scope="row">1</th>
+                    <th scope="row">${orderNumber}</th>
                     <td>${newPizza.size}</td>
                     <td>${newPizza.toppings}</td>
                     <td>${newPizza.crust}</td>
@@ -34,6 +39,13 @@ $(document).ready(function(){
     })
      $(".no").click(function(){
         $(".deliver").hide();
-         $("h5").text("Your order is ready,kindly come and pick it.") ;
+         $("h5").text("Your order is ready,total cost is, Ksh. " + totalCost) ;
+    })
+    $(".yes").click(function(){
+        $(".deliver").hide();
+        let location=prompt("Please enter your location");
+        $(".total").text("Total cost without delivery, Ksh. "+ totalCost)
+         $("h5").text("Your order will be delivered to " + location + ". Total cost is, Ksh. "  + parseInt(totalCost + 200));
+        
     })
 });
